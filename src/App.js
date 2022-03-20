dd2b7a624f880a8be28ec9dafe793e4eee8d25fb
Amit Yadav <samkr8991@gmail.com>
2022-03-20 18:19:06 +0530

    counter app with UseReducer Hook.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { type } from "@testing-library/user-event/dist/type";
-import React, { useState, useReducer } from "react";
+import React, { useReducer } from "react";
 import "./style.css";
 
 const initialState = 0;
@@ -42,3 +41,27 @@ const App = () => {
 };
 
 export default App;
+
+
+
+// NOTE: Same project but with useState
+
+// import React, { useState } from "react";
+// import "./style.css";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div className="Container">
+//       <h1>An App for Three-Counter-State</h1>
+//       <h2>{count}</h2>
+//       <button onClick={() => setCount(0)} className="button" id="reset">Reset Counter</button>
+//       <button onClick={() => (count >= 5 ? "" : setCount(count + 1))}  className="button" id="increment">Increment Number</button> 
+//       <br/>
+//       <button onClick={() => (count <= 1 ? "" : setCount(count - 1))} className="button" id="decrement">Decrement Number</button>
+//     </div>
+//   );
+// };
+
+// export default App;
